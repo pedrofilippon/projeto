262d67b8a5706ffeb55d2c5a6b2c85f6c784b28f
pedro <p.gustavofilippon@hotmail.com>
2014-04-22 09:26:08 -0300

    novo

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+	int main(){
+            int op;
+            switch(op){
+                case 0:
+					System.out.println("digite num");
+					
+                break;
+				case 1:
+				//primo
+				break;
+				case 2:
+				//imprime primo
+				break;
+				case 3:
+				//media entre os numeros
+				break;
+
+            }
+
+    }
+}
