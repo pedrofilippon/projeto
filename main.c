2ce20b458f4df112e274875e871b60fbe5b96770
EdirlanCensi <edirlan.censi2@gmail.com>
2014-04-22 12:26:57 -0300

    EdirlanCensi
    Bug função Primo Resolvido

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 void porcento(int a,int p){
 	int resut;
 	resut=a*p/100;
@@ -7,38 +9,40 @@ void porcento(int a,int p){
 
 int primo( int num){
 	int i;
-	for(i= 3; i < num-1; i++){
+	for(i= 2; i < num-1; i++){
 		 if(num%i==0) return 0;
 	}
 	return 1;
-
 }
 	int main(){
-            int op,a,p,bb;
-			printf("dite op\n");
-			scanf("%d",&op);
+		int op,a,p,bb;
+        int num;
+		printf("dite op\n");
+		scanf("%d",&op);
+
+		switch(op){
+			case 0:
 			printf("dite num\n");
 			scanf("%d",&a);
 			printf("dite porcento\n");
 			scanf("%d",&p);
-            switch(op){
-                case 0:
-                    porcento(a,p);
-                break;
-				case 1:
-				//primo
-				if(primo(num)) printf("Primo!");
-				else  printf("Nao Primo!");
-				
-				break;
-				case 2:
-				//imprime primo
-				break;
-				case 3:
-				//media entre os numeros
-				break;
+			porcento(a,p);
+			break;
+			
+			case 1:
+			//primo
+			printf("Digite num\n");
+			scanf("%d",&num);
+			if(primo(num)) printf("Primo!");
+			else  printf("Nao Primo!");
+			break;
+			
+			case 2:
+			//imprime primo
+			break;
+			case 3:
+			//media entre os numeros
+			break;
 
-            }
-
-
+		}
 }
