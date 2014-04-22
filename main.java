c1b81b46be9f7da16c6823d1b3e373bc1becaca4
pedro <p.gustavofilippon@hotmail.com>
2014-04-22 09:21:54 -0300

    estrutura

--- main.java
+++ main.java
@@ -3,7 +3,17 @@ class main{
             int op;
             switch(){
                 case 0:
+				//porcentagem
                 break;
+				case 1:
+				//primo
+				break;
+				case 2:
+				//imprime primo
+				break;
+				case 3:
+				//media entre os numeros
+				break;
 
             }
 
